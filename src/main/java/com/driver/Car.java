548a39a5e955bbd0e0382f90b974a9641b0372f1
Kashyap <bh.kashyap.b@gmail.com>
2023-10-04 13:29:09 +0530

    4th Commit

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -23,6 +23,18 @@ public class Car extends Vehicle {
         currentGear = 1;
     }
 
+    public void changeGear(int newGear) {
+        currentGear = newGear;
+
+        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+    }
+
+    public void changeSpeed(int newSpeed, int newDirection) {
+        move(newSpeed, newDirection);
+
+        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+
     public int getWheels() {
         return wheels;
     }
@@ -51,15 +63,5 @@ public class Car extends Vehicle {
         return seats;
     }
 
-    public void changeGear(int newGear) {
-        currentGear = newGear;
 
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
-    }
-
-    public void changeSpeed(int newSpeed, int newDirection) {
-        move(newSpeed, newDirection);
-
-        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
-    }
 }
