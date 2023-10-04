7329129842cd74a364535409a8b7a79c67a1fcff
Kashyap <bh.kashyap.b@gmail.com>
2023-10-04 17:55:25 +0530

    6th Commit

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -8,9 +8,8 @@ public class F1 extends Car {
     }
 
 
-
-    public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+    public void accelerate(int rate) {
+        int newSpeed = getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -21,31 +20,32 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        if (newSpeed == 0) {
             //Stop the car, set gear as 1
             stop();
-            this.changeGear(1);
+            changeGear(1);
         }
-        else if(newSpeed > 0 && newSpeed <= 50) {
-            this.changeGear(1);
-        }
-        else if (newSpeed > 50 && newSpeed <= 100) {
-            this.changeGear(2);
-        }
-        else if(newSpeed > 100 && newSpeed <= 150) {
-            this.changeGear(3);
-        }
-        else if(newSpeed > 150 && newSpeed <= 200) {
-            this.changeGear(4);
-        }
-        else if (newSpeed > 200 && newSpeed <= 250) {
-            this.changeGear(5);
-        }
-        else {
-            this.changeGear(6);
-        }
-        if(newSpeed > 0) {
-            this.changeSpeed(newSpeed, this.getCurrentDirection());
+        //for all other cases, change the gear accordingly
+
+        if (newSpeed > 0) {
+            changeSpeed(newSpeed, getCurrentDirection());
+
+            int gear;
+
+            if (newSpeed <= 50)
+                gear = 1;
+            else if (newSpeed <= 100)
+                gear = 2;
+            else if (newSpeed <= 150)
+                gear = 3;
+            else if (newSpeed <= 200)
+                gear = 4;
+            else if (newSpeed <= 250)
+                gear = 5;
+            else
+                gear = 6;
+
+            changeGear(gear);
         }
     }
 }
